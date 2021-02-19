f110d284ecedf149662c70667e9247538ec1bbde
Clumart.G <ktaog6@126.com>
2021-02-19 00:54:23 -0500

    update: fix history setting.

--- rhel7/sysinit-1.0.0-2.el7.noarch.sh
+++ rhel7/sysinit-1.0.0-2.el7.noarch.sh
@@ -178,8 +178,8 @@ echo "alias wgets='wget --no-check-certificate'" >> /etc/bashrc
 echo "alias vi='vim'" >> /etc/bashrc
 
 ##Set history
-echo "HISTSIZE=99999" > /etc/profile.d/history.sh
-echo 'HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S `whoami` "' > /etc/profile.d/history.sh
+echo "HISTSIZE=99999" >> /etc/profile.d/history.sh
+echo 'HISTTIMEFORMAT="%Y-%m-%d %H:%M:%S `whoami` "' >> /etc/profile.d/history.sh
 
 ##Set default service at poweron
 for i in `systemctl -t service list-unit-files |cut -d' ' -f1 |grep 'service$' |grep -v @`;do
